362c1943c5787079a88aade19befb0c4208262c2
harianchery <hari.anchery001@gmail.com>
2024-06-13 00:29:45 +0530

    navbar created and routed

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 import Add from './components/Add';
 import Search from './components/Search';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import View from './components/View';
+
 
 function App() {
   return (
     <div >
-      <Add/>
-      <Search/>
-      <View/>
+    <BrowserRouter>
+   <Routes>
+    <Route path='/' element={<Add/>}/>
+    <Route path='/search' element={<Search/>}/>
+    <Route path='/view' element={<View/>}/>
+   </Routes>
+   </BrowserRouter>
     </div>
   );
 }
